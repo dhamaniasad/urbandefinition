e7a800b377ec39c8512143fb4804fb36b331e16b
Asad Dhamani <dhamaniasad@gmail.com>
2015-06-28 20:11:57 +0530

    Fix, update version

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ install_requires = [
 setup(
     name='urbandefinition',
     install_requires=install_requires,
-    version=0.1,
+    version=0.2,
     description='Get Urban Dictionary definitions from the terminal',
     author='Asad Dhamani',
     author_email='dhamaniasad+code@gmail.com',
     url='https://github.com/dhamaniasad/urbandefinition',
     license='Unlicense',
-    py_modules=['magnetor'],
+    py_modules=['urbandefinition'],
     entry_points={
         'console_scripts': [
             'urban = urbandefinition:command_line_runner'
